6ff6f15bc1eeeb7e46e303787a4e4438d792bfac
Kiều Anh AI <anhntk1@unicloud.com.vn>
2023-07-31 15:31:33 +0700

    Add yolov8 detection backend

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# lbsdoatln
+# Yolov8 Detection Label Studio Active Learning
+
+## Setup
+
+```bash
+cd LabelStudio-Yolov8-Detection-Backend
+```
+
+- Install libraries
+
+```bash
+pip install -r requirements.txt
+```
+
+- Set environment variables
+
+```bash
+export LABEL_STUDIO_ML_BACKEND_V2=True
+```
+
+- Set path in yolov8.py, use absolute path
+
+```python 
+# labelstudio annotation: "image": "/data/upload/20/image1.png" or "image": "/data/local-files/?d=LABEL_STUDIO_LOCAL_FILES_DOCUMENT_ROOT/image1.png"
+LABELSTUDIO_PATH = '/data/upload/20' 
+DATASET_ROOT_DIR = '/home/user/apps/label-studio/media/upload/20' # folder where your images is store
+STORAGE_DIR = '/home/user/LabelStudio-Yolov8-Detection-Backend/data' # folder to save training artifacts
+MODEL_NAME = 'yolov8n_custom' # name your model
+MODEL_INIT = '/home/user/LabelStudio-Yolov8-Detection-Backend/model/yolov8n.pt' # model init
+MODEL_LATEST = f'/home/user/LabelStudio-Yolov8-Detection-Backend/runs/detect/{MODEL_NAME}/weights/best.pt' # latest model updated after each training
+DETECTION_YAML = '/home/user/LabelStudio-Yolov8-Detection-Backend/data/detection.yaml' # your yaml for training yolov8
+```
+
+- Setup yolov8 dataset yaml in data/detection.yaml
+
+```yaml 
+train: /home/user/LabelStudio-Yolov8-Detection-Backend/data/images
+val: /home/user/LabelStudio-Yolov8-Detection-Backend/data/images
+
+nc: 2
+names: ['table', 'signature']
+```
+
+## Create - launch - connect ML backend server 
+
+- Run in terminal
+
+```bash
+label-studio-ml init yolov8_backend --script yolov8.py --force
+
+label-studio-ml start yolov8_backend
+```
+
+- Connect Label Studio with ML backend
+
+![ML backend](assets/ml_backend.png)
+
---
+++ yolov8.py
@@ -0,0 +1,138 @@
+import os
+import shutil
+from glob import glob
+
+from ultralytics import YOLO
+from label_studio_ml.model import LabelStudioMLBase
+from label_studio_ml.utils import get_single_tag_keys
+
+LABELSTUDIO_PATH = '/data/upload/20'
+DATASET_ROOT_DIR = '/home/kieu/apps/label-studio/media/upload/20'
+STORAGE_DIR = '/home/kieu/Downloads/LabelStudio-Yolov8-Detection-Backend/data'
+MODEL_NAME = 'yolov8n_custom'
+MODEL_INIT = '/home/kieu/Downloads/LabelStudio-Yolov8-Detection-Backend/model/yolov8n.pt'
+MODEL_LATEST = f'/home/kieu/Downloads/LabelStudio-Yolov8-Detection-Backend/runs/detect/{MODEL_NAME}/weights/best.pt'
+DETECTION_YAML = '/home/kieu/Downloads/LabelStudio-Yolov8-Detection-Backend/data/detection.yaml'
+
+
+class SignatureDetectionAPI(LabelStudioMLBase):
+	"""This simple Label Studio ML backend demonstrates training & inference steps with a simple scenario:
+	on training: it gets the latest created annotation and stores it as "prediction model" artifact
+	on inference: it returns the latest annotation as a pre-annotation for every incoming task
+
+	When connected to Label Studio, this is a simple repeater model that repeats your last action on a new task
+	"""
+	def __init__(self, **kwargs):
+		super(SignatureDetectionAPI, self).__init__(**kwargs)
+		self.from_name, self.to_name, self.value, self.labels_in_config = get_single_tag_keys(
+			self.parsed_label_config, 'RectangleLabels', 'Image')
+		self.labels_in_config = list(self.labels_in_config)
+
+		if self.train_output:
+			self.model = YOLO(self.train_output['checkpoint'])
+		else:
+			self.model = YOLO(MODEL_INIT)
+
+	def predict(self, tasks, **kwargs):
+		""" This is where inference happens:
+			model returns the list of predictions based on input list of tasks
+
+			:param tasks: Label Studio tasks in JSON format
+		"""
+		image_urls = [task['data'][self.value] for task in tasks]
+		# image_url = image_urls[0].replace('/data/local-files/?d=', '/media/kieu/caithungrac/workspace/uni/')
+		image_url = image_urls[0].replace(LABELSTUDIO_PATH, DATASET_ROOT_DIR)
+		results = self.model.predict(image_url)
+
+		output_prediction = []
+		img_h, img_w = results[0].orig_shape[0], results[0].orig_shape[1]
+		model_label = results[0].names
+		for result in results[0]:
+			boxes = result.boxes.data.tolist()[0]
+			output_label = model_label[int(boxes[-1])]
+
+			if output_label not in self.labels_in_config:
+				print(output_label + ' label not found in project config.')
+				continue
+
+			output_prediction.append(
+				{
+					"to_name": self.to_name,
+					"from_name": self.from_name,
+					"type": "rectanglelabels",
+					"value": {
+						"x": boxes[0] / img_w * 100,
+						"y": boxes[1] / img_h * 100,
+						"width": (boxes[2] - boxes[0]) / img_w * 100,
+						"height": (boxes[3] - boxes[1]) / img_w * 100,
+						"rotation": 0,
+						"rectanglelabels": [
+							output_label
+						]
+					}
+				}
+			)
+		return [{
+			'result': output_prediction
+		}]
+	
+	def _create_dataset(self, completions: list):
+		# prepare storage folder
+		label_folder = os.path.join(STORAGE_DIR, 'labels')
+		image_folder = os.path.join(STORAGE_DIR, 'images')
+		[os.makedirs(subfolder, exist_ok=True) for subfolder in [label_folder, image_folder]]
+		train_images = glob(image_folder + '**.*')
+
+		for i in completions:
+			# save image path for list of image path
+			filename = i['data']['image'].replace(LABELSTUDIO_PATH, image_folder)
+			fileext = os.path.splitext(filename)[-1]
+			if filename not in train_images:
+				shutil.copy(filename.replace(image_folder, DATASET_ROOT_DIR), filename)
+
+			# remove old label file
+			labelname = filename.replace('/images/', '/labels/').replace(fileext, '.txt')
+			if os.path.exists(labelname):
+				os.remove(labelname)
+
+			# save annotation for label file
+			annotations = i['annotations'][0]['result']
+			for result in annotations:
+				value = result['value']
+				rectanglelabels = self.labels_in_config.index(value['rectanglelabels'][0])
+				
+				x = value['x']
+				y = value['y']
+				width = value['width']
+				height = value['height']
+
+				x_center = (x * 2 + width) / 200
+				y_center = (y * 2 + height) / 200
+				width_box = width / 100
+				height_box = height / 100
+
+				with open(labelname, "a") as f:
+					f.write(f'{rectanglelabels} {x_center} {y_center} {width_box} {height_box}\n')
+		
+
+	def fit(self, completions, workdir=None, 
+            batch_size=32, num_epochs=50, imgsz = 640, **kwargs):
+		"""
+		This method is called each time an annotation is created or updated
+		:param kwargs: contains "data" and "event" key, that could be used to retrieve project ID and annotation event type
+						(read more in https://labelstud.io/guide/webhook_reference.html#Annotation-Created)
+		:return: dictionary with trained model artefacts that could be used further in code with self.train_output
+		"""
+
+		print('Collecting dataset ...')
+		self._create_dataset(completions)
+
+		self.model.train(data = DETECTION_YAML,
+						imgsz = imgsz,
+						epochs = num_epochs,
+						batch = batch_size,
+						name = MODEL_NAME,
+						exist_ok = True,
+						augment=False)
+
+		return {'checkpoint': MODEL_LATEST}
